58cf674d929aa564cb5fed46e33a96dc6acc040f
DanielG-H <danielgarcia.alumno@gmail.com>
2020-12-07 16:28:15 -0800

    feat: add and configure the url of the landing page

--- toDo/views.py
+++ toDo/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+todos = [
+    {
+        'author': 'daniel garcia',
+        'title': 'my first to-do',
+        'description': 'a nice to-do',
+        'date_posted': '7/12/20',
+        'due_date': '8/12/20',
+    },
+]
+
+
+def home(request):
+    context = {
+        "todos": todos,
+    }
+    return render(request, 'toDo/home.html', context)
+
+
+# class PostListView(ListView):
+#     model = to_do
+#     template_name = html
+#     context_object_name = 'todos'
+#     ordering = ['-date_posted']
